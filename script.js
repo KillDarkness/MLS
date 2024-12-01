3ab9055554111ddfb8610289de2fa379e3acce71
KillDarkness <158506195+KillDarkness@users.noreply.github.com>
2024-12-01 19:25:50 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+// Função para mudar a mensagem ao clicar no botão
+function changeMessage() {
+  const messageElement = document.getElementById('message');
+  messageElement.innerText = 'Você clicou no botão! 🎉';
+}
